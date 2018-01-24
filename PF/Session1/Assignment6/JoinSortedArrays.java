fbcffa39e132a8a89c8b6dc37779014ef2bec10c
meta-anshul-agarwal <35560887+meta-anshul-agarwal@users.noreply.github.com>
2018-01-24 15:38:37 +0530

    Done changes in Assignment 6 session 1

--- PF/Session1/Assignment6/JoinSortedArrays.java
+++ PF/Session1/Assignment6/JoinSortedArrays.java
@@ -17,31 +17,27 @@ public class JoinSortedArrays {
 	 */
 	public int[] join(int a[], int asize, int b[], int bsize, int c[]){
 		
-		int counterA=0;
-		int counterB=0;
-		int counterC=0;
+		int counterA=0;		// initial counter of array a
+		int counterB=0;		// initial counter of array b
+		int counterC=0;		// initial counter of array c
 		
+		// merging arrays to c, putting smaller value. 
 		while(counterA < asize && counterB < bsize){
-			if(a[counterA] < b[counterB]){
+			if(a[counterA] <= b[counterB]){
 				c[counterC++] = a[counterA];
 				counterA++;
 			}	
-			else if(a[counterA] > b[counterB]){
-				c[counterC++] = b[counterB];
-				counterB++;
-			}
 			else{
-				c[counterC++] = a[counterA]; 
 				c[counterC++] = b[counterB];
-				counterA++;
 				counterB++;
 			}
+			
 		}
-		while(counterA < asize){
+		while(counterA < asize){              // add remaining elements of array a if there exists
 			c[counterC++] = a[counterA];
 			counterA++;
 		}
-		while(counterB < bsize){
+		while(counterB < bsize){	      // add remaining elements of array b if there exists
 			c[counterC++] = b[counterB];
 			counterB++;
 		}
