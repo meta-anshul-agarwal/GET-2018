3abc081efc6820d78abb777bce7cafa6f7d41fce
Anshul Agarwal <anshul.agarwal@metacube.com>
2018-01-30 10:18:57 +0530

    merging

---
+++ PF/Session2/UserException/IllegalExpressionException.java
@@ -0,0 +1,12 @@
+package PF.Session2.UserException;
+
+public  class IllegalExpressionException extends Exception {
+
+	public IllegalExpressionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
