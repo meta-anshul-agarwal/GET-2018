239b0ad68e121480b3ee8d036e6aacfc57e8cd6d
meta-anshul-agarwal <35560887+meta-anshul-agarwal@users.noreply.github.com>
2018-02-05 19:29:58 +0530

    OOPS session 3 Assignment

---
+++ oops/session3/State.java
@@ -0,0 +1,21 @@
+package session3;
+
+/**
+ * 
+ * @author Anshul Agarwal
+ * This class represents the various state of workflow
+ */
+public class State {
+	
+	//variable storing name of the workflow
+	String stateName;
+	
+	/**
+	 * constructor initializing statename
+	 * @param stateName name of the state
+	 */
+	State(String stateName){
+		this.stateName = stateName;
+	}
+	
+}
