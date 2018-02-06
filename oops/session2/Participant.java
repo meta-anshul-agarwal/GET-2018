0ebf3ffa45d843443e40837139893a55f4a5d86f
meta-anshul-agarwal <35560887+meta-anshul-agarwal@users.noreply.github.com>
2018-02-06 20:10:30 +0530

    Oops - session 2 assignment

---
+++ oops/session2/Participant.java
@@ -0,0 +1,30 @@
+package session2;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+/**
+ * this class represents the participants with their answers
+ * @author Anshul Agarwal
+ *
+ */
+public class Participant {
+	HashMap<Question, String> answerList = new LinkedHashMap<>();
+	
+	/**
+	 * method that adds question and answer to the hashmap
+	 * @param question - object of question type having the question
+	 * @param answer - answer of the participant for corresponding questions
+	 */
+	public void setParticipantAnswer (Question question, String answer) {
+		this.answerList.put (question, answer);
+	}
+
+	/**
+	 * method to gt answer list
+	 * @return answerList - list of answers with respect to question
+	 */
+	public HashMap<Question, String> getAnswerList () {
+		return this.answerList;
+	}
+}
